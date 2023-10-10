fd817d53d320ba0ee50529fd0ecb88a7742a1691
Vinícius Alves Borba <viniboal28@gmail.com>
2023-10-10 10:43:10 -0300

    feat: added db and user table

--- src/main/java/br/com/viniciusborba/tododolist/user/UserController.java
+++ src/main/java/br/com/viniciusborba/tododolist/user/UserController.java
@@ -1,5 +1,7 @@
 package br.com.viniciusborba.tododolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +10,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
+  @Autowired
+  private IUserRepository userRepository;
   
   @PostMapping("/")
-  public void create(@RequestBody UserModel userModel) {
-    System.out.println(userModel.name);
+  public ResponseEntity create(@RequestBody UserModel userModel) {
+    var user = this.userRepository.findByUsername(userModel.getUsername());
+
+    if (user != null) {
+      return ResponseEntity.status(400).body("Usuário já cadastrado");
+    }
+    
+    this.userRepository.save(userModel);
+    return ResponseEntity.status(200).body(null);
   }
 }
--- src/main/java/br/com/viniciusborba/tododolist/user/UserModel.java
+++ src/main/java/br/com/viniciusborba/tododolist/user/UserModel.java
@@ -1,9 +1,34 @@
 package br.com.viniciusborba.tododolist.user;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity(name= "tb_users")
 public class UserModel {
-    
-  public String username;
-  public String name;
-  public String email;
-  public String password;
+  
+  @Id
+  @GeneratedValue(generator = "UUID")
+  private UUID id;
+
+  @Column(unique = true)
+  private String username;
+  
+  private String name;
+  
+  @Column(unique = true)
+  private String email;
+  
+  private String password;
+
+  @CreationTimestamp
+  private LocalDateTime createdAt;
 }
